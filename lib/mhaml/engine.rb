03eaa5ccc4cd7a51569dd494ce962aa172dd00da
BlueHotDog <dannifriedland@gmail.com>
2012-05-17 22:02:48 +0300

    fixing typo

--- lib/mhaml/engine.rb
+++ lib/mhaml/engine.rb
@@ -2,7 +2,7 @@ module MHaml
   class Engine < ::Rails::Engine
     initializer "sprockets.mhaml_rails", :after => "sprockets.environment", :group => :all do |app|
       next unless app.assets
-      app.assets.register_engine(".#{MHaml.template_extension}", MHaml::MHamlTemplate)
+      app.assets.register_engine(".#{MHaml.template_extension}", MHamlTemplate)
       app.config.assets.paths << MHaml.template_base_path
     end
   end
