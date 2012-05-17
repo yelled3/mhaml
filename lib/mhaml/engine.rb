543ab8e082d459ef5593b447a2463de7c2d46248
BlueHotDog <dannifriedland@gmail.com>
2012-05-17 21:59:59 +0300

    fixing typo

--- lib/mhaml/engine.rb
+++ lib/mhaml/engine.rb
@@ -1,12 +1,8 @@
 module MHaml
   class Engine < ::Rails::Engine
-    #config.before_configuration do |app|
-    #  app.paths['app/views'] << MHaml.template_base_path
-    #end
-
     initializer "sprockets.mhaml_rails", :after => "sprockets.environment", :group => :all do |app|
       next unless app.assets
-      app.assets.register_engine(".#{MHaml.template_extension}", Tilt)
+      app.assets.register_engine(".#{MHaml.template_extension}", MHaml::MHamlTemplate)
       app.config.assets.paths << MHaml.template_base_path
     end
   end
